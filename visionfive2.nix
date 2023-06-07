82e11c0c89d265b089cafe051479b95b9c08365e
Neil Skinner <reqpro@requaos.com>
2023-06-07 08:30:53 +0000

    some pwd

--- visionfive2.nix
+++ visionfive2.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }: {
 
   nixpkgs = {
-    localSystem.config = "x86_64-unknown-linux-gnu";
-    crossSystem.config = "riscv64-unknown-linux-gnu";
+    localSystem.config = "riscv64-unknown-linux-gnu";
   };
 
   virtualisation.docker.enable = true;
+  users.users.root.hashedPassword = "$y$j9T$udmWOUL83BI/zSUuqJOXR.$8xR73OTkV52DQVdp6PspvROhLzG8Mgj3VQjG8AOub34";
+  users.users.nixos.hashedPassword = "$y$j9T$WKj3UyDIuS1i5jl8u62Gm0$trGjHf0T4ob87gdP.qQvwKIjCND.r8ckCdupE1yLgy8";
 
-  environment.systemPackages = with pkgs;[ neofetch iperf3 ];
+  environment.systemPackages = with pkgs;[ neofetch iperf3 htop git ];
 
 }
